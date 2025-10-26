4d9303b66119ab87a06ed374b224da5c5bd2a837
Marcel Meyer <23430036+marcel-b-meyer1987@users.noreply.github.com>
2025-10-26 21:09:23 +0100

    add HTML skeleton and basic styling

--- calc.js
+++ calc.js
@@ -1,3 +1,34 @@
+
+//ROBERT MEYER
+
+"use strict";
+
+// initialize basic variables for all operations
+let operand1 = 0;
+let operand2 = 0;
+let operator = "";
+
+
+// main function of the app
+function operate(operand1, operation, operand2) {
+    switch(operation) {
+        case "+":
+            return add(operand1, operand2);
+            break;
+        case "-":
+            return subtract(operand1, operand2);
+            break;
+        case "*":
+            return multiply(operand1, operand2);
+            break;
+        case "/":
+            return divide(operand1, operand2);
+            break;
+    }
+}
+
+
+// functions for indivicual mathematical operations
 function add(a, b) {
     return a + b;
 }
